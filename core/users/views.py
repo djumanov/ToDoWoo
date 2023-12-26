00c043834d5953ce65bdb173abf328aadefbeb2d
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-26 15:21:35 +0500

    create and get todos

--- core/users/views.py
+++ core/users/views.py
@@ -1,9 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.authentication import BaseAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authtoken.models import Token
 from rest_framework import status
 
 from .serializers import SignUpSerializer
