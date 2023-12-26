2e09a86f120f55979e3619b67bac137002aaeea1
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-26 16:41:14 +0500

    testst

--- core/todos/tests.py
+++ core/todos/tests.py
@@ -1,3 +1,60 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from .models import Todo
 
-# Create your tests here.
+
+class TodoTests(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.client.force_authenticate(user=self.user)
+        self.todo_data = {
+            'title': 'Test Todo',
+            'memo': 'Test memo',
+        }
+
+    def test_todo_create_view(self):
+        url = reverse('todo_create')
+        response = self.client.post(url, self.todo_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertEqual(Todo.objects.get().title, 'Test Todo')
+        self.assertEqual(Todo.objects.get().user, self.user)
+
+    def test_todo_create_view_unauthenticated(self):
+        self.client.force_authenticate(user=None)
+        url = reverse('todo_create')
+        response = self.client.post(url, self.todo_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(Todo.objects.count(), 0)
+
+    def test_todo_detail_view(self):
+        todo = Todo.objects.create(
+            title='Test Todo',
+            memo='Test memo',
+            user=self.user
+        )
+        url = reverse('todo_detail', args=[todo.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Test Todo')
+        self.assertEqual(response.data['user'], self.user.id)
+
+    def test_todo_detail_view_unauthenticated(self):
+        todo = Todo.objects.create(
+            title='Test Todo',
+            memo='Test memo',
+            user=self.user
+        )
+        self.client.force_authenticate(user=None)
+        url = reverse('todo_detail', args=[todo.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
--- core/users/tests.py
+++ core/users/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+
+class SignUpViewTests(APITestCase):
+    def setUp(self):
+        self.signup_url = reverse('signup')
+        self.user_data = {
+            'username': 'testuser',
+            'password': 'testpassword',
+        }
+
+    def test_signup_success(self):
+        response = self.client.post(self.signup_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(get_user_model().objects.count(), 1)
+        self.assertEqual(get_user_model().objects.get().username, 'testuser')
+
+    def test_signup_missing_required_fields(self):
+        incomplete_data = {
+            'username': 'testuser',
+        }
+
+        response = self.client.post(self.signup_url, incomplete_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(get_user_model().objects.count(), 0)
+
+    def test_signup_existing_user(self):
+        get_user_model().objects.create_user(username='testuser', password='testpassword')
+
+        response = self.client.post(self.signup_url, self.user_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(get_user_model().objects.count(), 1)
