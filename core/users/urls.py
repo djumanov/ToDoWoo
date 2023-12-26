4c67c1dbcaaceae156fc80d3018d8cc6ab3a6cea
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-26 15:09:14 +0500

    signin api

--- core/users/urls.py
+++ core/users/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from .views import SignUpView
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('signin/', obtain_auth_token, name='signin'),
 ]
